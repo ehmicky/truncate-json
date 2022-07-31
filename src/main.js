c2be05c66e660e552d1d15e6e7786cbf1c23e5bb
ehmicky <ehmicky@gmail.com>
2022-07-31 23:20:54 +0200

    Split file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-import { recurseArray } from './array.js'
-import { recurseObject } from './object.js'
-import { addSize, getValueSize } from './size.js'
+import { truncateValue } from './value.js'
 
 // Truncate a JSON string
 export default function truncateJson(jsonString, maxSize) {
@@ -26,64 +24,4 @@ const parseJson = function (jsonString) {
   } catch (error) {
     throw new Error(`Invalid JSON string: "${jsonString}"\n${error.message}`)
   }
-}
-
-// Recurse over plain objects and arrays.
-// We use a depth-first traversal.
-//  - I.e. parent, then children, then siblings
-//  - This works better with `maxSize`
-//     - This allows stopping logic when `maxSize` is reached, resulting in
-//       better performance
-//     - This favors removing fewer big fields instead of more small fields,
-//       resulting in fewer `omittedProps`
-//     - This favors maximizing the number of fields within the allowed
-//       `maxSize`
-//  - This is easier to implement
-const truncateValue = function ({ value, omittedProps, path, size, maxSize }) {
-  const increment = getValueSize(value)
-  const {
-    size: sizeA,
-    stop,
-    omittedProps: omittedPropsA,
-  } = addSize({
-    size,
-    increment,
-    maxSize,
-    omittedProps,
-    path,
-    value,
-  })
-  return stop
-    ? { value: undefined, size: sizeA, omittedProps: omittedPropsA }
-    : recurseValue({
-        value,
-        omittedProps: omittedPropsA,
-        path,
-        size: sizeA,
-        maxSize,
-      })
-}
-
-const recurseValue = function ({ value, omittedProps, path, size, maxSize }) {
-  if (typeof value !== 'object' || value === null) {
-    return { value, size, omittedProps }
-  }
-
-  return Array.isArray(value)
-    ? recurseArray({
-        array: value,
-        omittedProps,
-        path,
-        size,
-        maxSize,
-        truncateValue,
-      })
-    : recurseObject({
-        object: value,
-        omittedProps,
-        path,
-        size,
-        maxSize,
-        truncateValue,
-      })
 }
---
+++ src/value.js
@@ -0,0 +1,70 @@
+import { recurseArray } from './array.js'
+import { recurseObject } from './object.js'
+import { addSize, getValueSize } from './size.js'
+
+// Truncate a value to fit within a specific JSON size
+export const truncateValue = function ({
+  value,
+  omittedProps,
+  path,
+  size,
+  maxSize,
+}) {
+  const increment = getValueSize(value)
+  const {
+    size: sizeA,
+    stop,
+    omittedProps: omittedPropsA,
+  } = addSize({
+    size,
+    increment,
+    maxSize,
+    omittedProps,
+    path,
+    value,
+  })
+  return stop
+    ? { value: undefined, size: sizeA, omittedProps: omittedPropsA }
+    : recurseValue({
+        value,
+        omittedProps: omittedPropsA,
+        path,
+        size: sizeA,
+        maxSize,
+      })
+}
+
+// Recurse over plain objects and arrays.
+// We use a depth-first traversal.
+//  - I.e. parent, then children, then siblings
+//  - This works better with `maxSize`
+//     - This allows stopping logic when `maxSize` is reached, resulting in
+//       better performance
+//     - This favors removing fewer big fields instead of more small fields,
+//       resulting in fewer `omittedProps`
+//     - This favors maximizing the number of fields within the allowed
+//       `maxSize`
+//  - This is easier to implement
+const recurseValue = function ({ value, omittedProps, path, size, maxSize }) {
+  if (typeof value !== 'object' || value === null) {
+    return { value, size, omittedProps }
+  }
+
+  return Array.isArray(value)
+    ? recurseArray({
+        array: value,
+        omittedProps,
+        path,
+        size,
+        maxSize,
+        truncateValue,
+      })
+    : recurseObject({
+        object: value,
+        omittedProps,
+        path,
+        size,
+        maxSize,
+        truncateValue,
+      })
+}
