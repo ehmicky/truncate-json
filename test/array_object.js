5abf684373eb978340b930a41ae8b338c8f180f6
ehmicky <ehmicky@gmail.com>
2022-07-31 23:34:35 +0200

    Add more tests

---
+++ test/array_object.js
@@ -0,0 +1,51 @@
+import test from 'ava'
+import { each } from 'test-each'
+import truncateJson from 'truncate-json'
+
+const bigStringMaxSize = 1e2
+const stringSize = 1e3
+const bigString = 'a'.repeat(stringSize)
+
+each(
+  [
+    {
+      input: { one: bigString, prop: true },
+      output: { prop: true },
+      path: ['one'],
+    },
+    { input: [bigString, true], output: [true], path: [0] },
+  ],
+  ({ title }, { input, output, path }) => {
+    test(`Omitted values are filtered and do not count towards maxSize | ${title}`, (t) => {
+      const inputString = JSON.stringify(input)
+      const outputString = JSON.stringify(output)
+      t.deepEqual(truncateJson(inputString, bigStringMaxSize), {
+        jsonString: outputString,
+        omittedProps: [{ path, value: bigString }],
+      })
+    })
+  },
+)
+
+each(
+  [
+    {
+      input: { one: { two: { three: true } } },
+      output: { one: {} },
+      path: ['one', 'two'],
+      value: { three: true },
+    },
+    { input: [[[true]]], output: [[]], path: [0, 0], value: [true] },
+  ],
+  ({ title }, { input, output, path, value }) => {
+    test(`Do not recurse on big fields | ${title}`, (t) => {
+      const inputString = JSON.stringify(input)
+      const outputString = JSON.stringify(output)
+      const maxSize = outputString.length
+      t.deepEqual(truncateJson(inputString, maxSize), {
+        jsonString: outputString,
+        omittedProps: [{ path, value }],
+      })
+    })
+  },
+)
