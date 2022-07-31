5786c69e3bfffaecda59ba5503f02cc31936a105
ehmicky <ehmicky@gmail.com>
2022-07-31 13:21:51 +0200

    Improve performance

--- src/length.js
+++ src/length.js
@@ -19,12 +19,14 @@ export const stringByteSlice = function (string, start, end) {
   const bytes = stringToBytes(string)
   const truncatedBytes = bytes.slice(start, end)
   const truncatedString = bytesToString(truncatedBytes)
-  return truncatedString.replace(INVALID_END_CHARS, '')
+  return truncatedString.endsWith(INVALID_END_CHAR)
+    ? truncatedString.slice(0, -1)
+    : truncatedString
 }
 
 // The truncation might happen in the middle of a multibyte Unicode sequence,
 // which is then replaced by \ufffd by TextDecoder. We trim it.
-const INVALID_END_CHARS = /\uFFFD$/u
+const INVALID_END_CHAR = '\uFFFD'
 
 // Turn a string into a UTF-8 bytes array
 const stringToBytes = function (string) {
