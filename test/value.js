7c59a24c662b7dfa29bf6531fa103239bfd465b5
ehmicky <ehmicky@gmail.com>
2022-07-31 23:33:53 +0200

    Merge files

--- test/value.js
+++ test/value.js
@@ -37,3 +37,29 @@ each(
     })
   },
 )
+
+each(
+  [
+    {
+      input: { one: { two: { three: true, four: true } } },
+      output: { one: { two: { three: true } } },
+      path: ['one', 'two', 'four'],
+    },
+    {
+      input: { one: { four: true, two: { three: true, four: true } } },
+      output: { one: { four: true, two: { three: true } } },
+      path: ['one', 'two', 'four'],
+    },
+  ],
+  ({ title }, { input, output, path }) => {
+    test(`Truncates in a depth-first manner | ${title}`, (t) => {
+      const jsonString = JSON.stringify(input)
+      const outputString = JSON.stringify(output)
+      const maxSize = outputString.length
+      t.deepEqual(truncateJson(jsonString, maxSize), {
+        jsonString: outputString,
+        omittedProps: [{ path, value: true }],
+      })
+    })
+  },
+)
