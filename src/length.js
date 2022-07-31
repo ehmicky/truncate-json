ddde013f24943c2fdd29c704ef675a358078d19e
ehmicky <ehmicky@gmail.com>
2022-07-31 11:46:00 +0200

    Improve performance

--- src/length.js
+++ src/length.js
@@ -6,8 +6,13 @@
 // length like `string.length`.
 export const getJsonLength = function (value) {
   const jsonString = JSON.stringify(value)
-  return stringToBytes(jsonString).length
+  return NO_STRING_JSON_TYPES.has(typeof value)
+    ? jsonString.length
+    : stringToBytes(jsonString).length
 }
+
+// For those types, the character length is the same as the UTF-8 byte length
+const NO_STRING_JSON_TYPES = new Set(['null', 'number', 'boolean'])
 
 // Turn a string into a UTF-8 bytes array
 export const stringToBytes = function (string) {
