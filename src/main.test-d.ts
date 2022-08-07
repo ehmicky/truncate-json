f69d0398c86cd95085b6a2d5eef5fdd13769792c
ehmicky <ehmicky@gmail.com>
2022-08-07 15:24:06 +0200

    Improve type tests

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -3,5 +3,6 @@ import { expectType, expectError } from 'tsd'
 import truncateJson from './main.js'
 
 expectType<string>(truncateJson('{}', 2))
+expectError(truncateJson({}))
 expectError(truncateJson({}, 2))
 expectError(truncateJson('{}', '2'))
